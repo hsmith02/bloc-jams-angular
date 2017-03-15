53851e9bb2f993ab3582432abaabc2e7e4a60adf
Heather Smith <hsmith.02@outlook.com>
2017-03-15 13:28:06 -0700

    checkpoint-8 in progress

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,8 +2,6 @@
     function SongPlayer() {
         var SongPlayer = {};
         
-        var currentSong = null;
-        
         /**
         * @desc Buzz object audio file
         * @type {Object}
@@ -20,7 +18,7 @@
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
             
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -28,7 +26,7 @@
                 preload: true
             });
             
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
         
         /**
@@ -42,6 +40,8 @@
             song.playing = true;
         };
         
+        SongPlayer.currentSong = null;
+        
         /**
         * @function SongPlayer.play
         * @desc if a song other than the current song or a paused song is clicked, play the clicked song
@@ -49,11 +49,12 @@
         */
         
         SongPlayer.play = function(song) {
-            if (currentSong !== song) {
+            song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
                 setSong(song);
                 playSong(song);
                 
-            } else if (currentSong === song) {
+            } else if (SongPlayer.currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
                 }
@@ -67,6 +68,7 @@
         */
         
         SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.Playing = false;
         };
